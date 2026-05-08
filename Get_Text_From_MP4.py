ca6975894913b3ae15f2e97470bcab4408163bf0
emotionnlpdataset <shawnjdemello@gmail.com>
2026-05-08 16:35:30 -0400

    Update Get_Text_From_MP4.py

--- Get_Text_From_MP4.py
+++ Get_Text_From_MP4.py
@@ -18,16 +18,10 @@ folder = "C:/Users/User/OneDrive/Documents/Research Project Data - Copy/"
 
 list1 = []
 i = 0
-trim_to_longest_scene = True
 for file in os.listdir(folder):
     print(f"{i+1}) {file}")
     temp_file_path = os.path.join(folder, file)
-    if trim_to_longest_scene is True:
-        start_time, end_time = find_timestamps_longest_scene(temp_file_path)
-        print(start_time, end_time)
-        config = aai.TranscriptionConfig(speaker_labels=True, audio_start_from=start_time*1000, audio_end_at=end_time*1000)
-    else:
-        config = aai.TranscriptionConfig(speaker_labels=True)
+    config = aai.TranscriptionConfig(speaker_labels=True)
     transcriber = aai.Transcriber()
     temp_transcript = transcriber.transcribe(temp_file_path, config=config)
     temp_transcript_text = temp_transcript.text
@@ -38,10 +32,7 @@ for file in os.listdir(folder):
     i += 1
 
 dataframe1 = pd.DataFrame(list1, columns=["Video File Name", "Video Duration", "Transcript Text"])
-if trim_to_longest_scene is True:
-    export_file = "C:/Users/User/Downloads/ResearchProjectTrimmedText.csv"
-else:
-    export_file = "C:/Users/User/Downloads/ResearchProjectText.csv"
+export_file = "C:/Users/User/Downloads/ResearchProjectText.csv"
 dataframe1.to_csv(export_file)
 
 
