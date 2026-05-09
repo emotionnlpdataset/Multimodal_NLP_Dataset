d13c0b9ad485ccb0e57610f2fa5e29e7f97c7946
emotionnlpdataset <shawnjdemello@gmail.com>
2026-05-09 14:15:11 -0400

    Update Multimodal.py

--- Multimodal.py
+++ Multimodal.py
@@ -16,8 +16,8 @@ path_to_folder = "path/to/folder/"
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--emotions_task", action="store_true", help="True if running emotional classification task; False if running emotional dimensional classification task")
-    parser.add_argument("--path_to_folder", action="store_true", help="Path to working directory")
+    parser.add_argument("--emotions_task", action="store_true", required = True, help="True if running emotional classification task; False if running emotional dimensional classification task")
+    parser.add_argument("--path_to_folder", action="store_true", required = True, help="Path to working directory")
 
     args = parser.parse_args()
 
