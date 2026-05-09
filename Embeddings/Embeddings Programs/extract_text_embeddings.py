f7ff567898a827d6773c7570c6b851af88c3258a
emotionnlpdataset <shawnjdemello@gmail.com>
2026-05-08 20:45:00 -0400

    Update extract_text_embeddings.py

--- Embeddings/Embeddings Programs/extract_text_embeddings.py
+++ Embeddings/Embeddings Programs/extract_text_embeddings.py
@@ -13,6 +13,37 @@ from natsort import natsorted
 from itertools import chain
 
 
+def get_corresponding_data(video_number, input_ids, attention_masks):
+    input_id = input_ids[video_number]
+    attention_mask = attention_masks[video_number]
+
+    labels_file = "C:/Users/User/PycharmProjects/Research Project/New_Labels_By_Classification_Emotions_Threshold15.npy"
+    labels_data = np.load(labels_file)
+    label_clip = labels_data[video_number]
+    label_clip = label_clip.astype(float)
+
+    condition_label_file = "C:/Users/User/PycharmProjects/Research Project/Condition_Labels.csv"
+    condition_label = pd.read_csv(condition_label_file)
+    cond_label = condition_label['Neurodivergent'].loc[video_number]
+    # Yes (1): Autism/Neurodivergent, No (0): Normal/Neurotypical
+
+    return input_id, attention_mask, label_clip, cond_label
+
+
+def make_whole_dataset(input_ids, attention_masks):
+    whole_text_input_ids_list = []
+    whole_attention_masks_list = []
+    whole_label_list = []
+    whole_condition_list = []
+    for i in range(1000):
+        text_input_id, attention_mask, new_label_clip, condition_label = get_corresponding_data(i, input_ids, attention_masks)
+        whole_text_input_ids_list.append(text_input_id)
+        whole_attention_masks_list.append(attention_mask)
+        whole_label_list.append(new_label_clip)
+        whole_condition_list.append(condition_label)
+    return whole_text_input_ids_list, whole_attention_masks_list, whole_label_list, whole_condition_list
+
+
 emotions_category = True
 if emotions_category is True:
     num_epochs = 20
